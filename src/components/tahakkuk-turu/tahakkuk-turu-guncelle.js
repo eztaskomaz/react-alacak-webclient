cd7556868e10c2dfce0f18d00eaa7643592d1c58
eztaskomaz <eztaskomaz@gmail.com>
2021-01-02 17:15:49 +0300

    tahakkuk turu guncelle fonksiyonu eklendi

--- src/components/tahakkuk-turu/tahakkuk-turu-guncelle.js
+++ src/components/tahakkuk-turu/tahakkuk-turu-guncelle.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import {
-    getTargetPathValue,
-    handleTargetPathValueChange, YteEnumInput, YteForm,
+    getTargetPathValue, handleRemoteCall,
+    handleTargetPathValueChange, YteCheckboxInput, YteEnumInput, YteForm,
     YteFormElement, YteMoneyInput, YteNumberInput, YteSelectInput,
     YteTextInput,
     YteUpdateComponent,
@@ -10,67 +10,76 @@ import {
 
 class TahakkukTuruGuncelle extends Component {
 
+    state = {
+        alacakKategorisiOptions: []
+    }
+
     validations = {
-        "state.item.name": {length: {gt: 2}},
-        "state.item.seller": {presence: true},
-        "state.item.price": {money: {ge: 2000}},
-        "state.item.remainingCount": {number: {le: 4000}},
-        "state.item.categories": {presence: true}
+        "state.item.ad": {presence: true, length: {le: 256, ge: 1}},
+        "state.item.aciklama": {presence: true, length: {le: 1024, mgein: 1}},
+        "state.item.oncelik": {presence: true, number: {onlyInteger: true, ge: 0, le: 2147483647}},
+        "state.item.maxTaksitYili": {number: {onlyInteger: true, ge: 0, le: 32767, allowBlank: true}},
+        "state.item.alacakKategorisiId": {presence: true},
+        "state.item.gelirIdaresineBildirmeSuresi": {number: {onlyInteger: true, ge: 0, le: 32767, allowBlank: true}},
+        "state.item.olusturulacakTahakkukTipi": {presence: true}
+    }
+
+    componentDidMount() {
+        handleRemoteCall({
+            method: "get",
+            targetUrl: "/tahakkukTuru/getAlacakKategorisiList",
+            afterOperationSucceeded: (data) => {
+                console.log(data);
+                this.setState({alacakKategorisiOptions: data});
+            }
+        });
     }
 
     render() {
         return <YteValidator target={this}>
             {
                 ({error, hasError}) => {
-                    return <YteUpdateComponent history={this.props.history} confirmDisabled={hasError}
-                                               location={this.props.location} getItemParams={this.props.getItemParams}
-                                               target={this} getUrl={"/tahakkuk-turu/get"} updateUrl={"/tahakkuk-turu/update"}>
+                    return <YteUpdateComponent history={this.props.history} confirmDisabled={hasError} location={this.props.location} getItemParams={this.props.getItemParams}
+                                               target={this} getUrl={"/tahakkukTuru/get"} updateUrl={"/tahakkukTuru/guncelle"}>
                         <YteForm>
-                            <YteFormElement sectionId={1} labelId="nameLabel" label="tahakkuk-turu.name" error={error}
-                                            errorPath="state.item.name">
-                                <YteTextInput value={getTargetPathValue(this, "state.item.name")}
-                                              onChange={newValue => handleTargetPathValueChange({
-                                                  target: this,
-                                                  path: "item.name",
-                                                  value: newValue
-                                              })}/>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.ad" label="tahakkuk-turu.ad" error={error} errorPath="state.item.ad">
+                                <YteTextInput value={getTargetPathValue(this, "state.item.ad")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.ad",value: newValue})}/>
                             </YteFormElement>
-                            <YteFormElement sectionId={1} labelId="sellerLabel" label="tahakkuk-turu.seller" error={error}
-                                            errorPath="state.item.seller">
-                                <YteSelectInput value={getTargetPathValue(this, "state.item.seller")}
-                                                onChange={newValue => handleTargetPathValueChange({
-                                                    target: this,
-                                                    path: "item.seller",
-                                                    value: newValue
-                                                })} options={["Dell", "Apple", "Faber Castell", "Microsoft"]}/>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.aciklama" label="tahakkuk-turu.aciklama" error={error} errorPath="state.item.aciklama">
+                                <YteTextInput value={getTargetPathValue(this, "state.item.aciklama")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.aciklama",value: newValue})}/>
                             </YteFormElement>
-                            <YteFormElement sectionId={1} labelId="priceLabel" label="tahakkuk-turu.price" error={error}
-                                            errorPath="state.item.price">
-                                <YteMoneyInput value={getTargetPathValue(this, "state.item.price")}
-                                               onChange={newValue => handleTargetPathValueChange({
-                                                   target: this,
-                                                   path: "item.price",
-                                                   value: newValue
-                                               })}/>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.alacakKategorisi" label="tahakkuk-turu.alacakKategorisi" error={error} errorPath="state.item.alacakKategorisiId">
+                                <YteSelectInput displayProperty="ad" valueProperty="id"
+                                                value={getTargetPathValue(this, "state.item.alacakKategorisiId")}
+                                                onChange={newValue => handleTargetPathValueChange({target:this, path:"item.alacakKategorisiId",value: newValue})}
+                                                options={this.state.alacakKategorisiOptions}/>
                             </YteFormElement>
-                            <YteFormElement sectionId={1} labelId="remainingCountLabel" label="tahakkuk-turu.remainingCount"
-                                            error={error} errorPath="state.item.remainingCount">
-                                <YteNumberInput value={getTargetPathValue(this, "state.item.remainingCount")}
-                                                onChange={newValue => handleTargetPathValueChange({
-                                                    target: this,
-                                                    path: "item.remainingCount",
-                                                    value: newValue
-                                                })}/>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.oncelik" label="tahakkuk-turu.oncelik" error={error} errorPath="state.item.oncelik">
+                                <YteNumberInput value={getTargetPathValue(this, "state.item.oncelik")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.oncelik",value: newValue})}/>
                             </YteFormElement>
-                            <YteFormElement sectionId={1} labelId="categoriesLabel" label="tahakkuk-turu.categories"
-                                            error={error} errorPath="state.item.categories">
-                                <YteEnumInput enumName="EnumCategories"
-                                              value={getTargetPathValue(this, "state.item.categories")}
-                                              onChange={newValue => handleTargetPathValueChange({
-                                                  target: this,
-                                                  path: "item.categories",
-                                                  value: newValue
-                                              })}/>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.maxTaksitYili" label="tahakkuk-turu.maxTaksitYili" error={error} errorPath="state.item.maxTaksitYili">
+                                <YteNumberInput value={getTargetPathValue(this, "state.item.maxTaksitYili")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.maxTaksitYili",value: newValue})}/>
+                            </YteFormElement>
+                            <YteFormElement sectionId={5} labelId="tahakkuk-turu.gelirIdaresineBildirmeSuresi" label="tahakkuk-turu.gelirIdaresineBildirmeSuresi" error={error} errorPath="state.item.gelirIdaresineBildirmeSuresi">
+                                <YteNumberInput value={getTargetPathValue(this, "state.item.gelirIdaresineBildirmeSuresi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.gelirIdaresineBildirmeSuresi",value: newValue})}/>
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.olusturulacakTahakkukTipi" label="tahakkuk-turu.olusturulacakTahakkukTipi" error={error} errorPath="state.item.olusturulacakTahakkukTipi">
+                                <YteEnumInput enumName="OlusturulacakTahakkukTipi" value={getTargetPathValue(this, "state.item.olusturulacakTahakkukTipi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.olusturulacakTahakkukTipi",value: newValue})}/>
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.sayistayIlamiTuruMu" label="tahakkuk-turu.sayistayIlamiTuruMu"  >
+                                <YteCheckboxInput value={getTargetPathValue(this, "state.item.sayistayIlamiTuruMu")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.sayistayIlamiTuruMu",value: newValue})} />
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.kismenKapatilabilirMi" label="tahakkuk-turu.kismenKapatilabilirMi"  >
+                                <YteCheckboxInput value={getTargetPathValue(this, "state.item.kismenKapatilabilirMi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.kismenKapatilabilirMi",value: newValue})} />
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.kismenTaksitKapatilabilirMi" label="tahakkuk-turu.kismenTaksitKapatilabilirMi"  >
+                                <YteCheckboxInput value={getTargetPathValue(this, "state.item.kismenTaksitKapatilabilirMi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.kismenTaksitKapatilabilirMi",value: newValue})} />
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.etahsilatYapilabilirMi" label="tahakkuk-turu.etahsilatYapilabilirMi"  >
+                                <YteCheckboxInput value={getTargetPathValue(this, "state.item.etahsilatYapilabilirMi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.etahsilatYapilabilirMi",value: newValue})} />
+                            </YteFormElement>
+                            <YteFormElement sectionId={6} labelId="tahakkuk-turu.pesinatliMi" label="tahakkuk-turu.pesinatliMi"  >
+                                <YteCheckboxInput value={getTargetPathValue(this, "state.item.pesinatliMi")} onChange={newValue => handleTargetPathValueChange({target:this, path:"item.pesinatliMi",value: newValue})} />
                             </YteFormElement>
                         </YteForm>
                     </YteUpdateComponent>
